adf455bc16ace0adb7c63e1f1d6cf51c83f98123
Wisdom-Kunambi <kunambiwaziri1@gmail.com>
2024-07-15 20:40:19 +0300

    add home.blade.php file, modify welcome.blade.php and modify web.php

---
+++ resources/views/home.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Testing 123</title>
+</head>
+<body>
+    <div style="border: 3px solid black">
+        <h2>Register</h2>
+        <form action="">
+            <input type="text" placceholder="Name">
+            <input type="text" placceholder="Email">
+            <input type="password" placceholder="Password">
+            <button>Register</button>
+        </form>
+
+    </div>
+</body>
+</html>
