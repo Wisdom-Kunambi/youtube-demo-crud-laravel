972397c528150c9cc400a9249c2e0d921dc765c3
Wisdom-Kunambi <kunambiwaziri1@gmail.com>
2024-07-15 22:08:25 +0300

    add changes in the home.blade.php and web.php files

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,7 +9,8 @@
 <body>
     <div style="border: 3px solid black">
         <h2>Register</h2>
-        <form action="">
+        <form action="/register" method="post">
+            @csrf
             <input type="text" placceholder="Name">
             <input type="text" placceholder="Email">
             <input type="password" placceholder="Password">
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+Route::get('/register', function () {  
+    return "Thanks for registering!";               
+});
